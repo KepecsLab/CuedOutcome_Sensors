1be7c769a66859e761d67a82fb60de0b05cae578
QuentinNeuro <quentin.chevy@gmail.com>
2018-10-25 14:42:37 -0400

    move evernote stuff to Bpod-Analysis in photometry launcher.

--- UserKillScript.m
+++ UserKillScript.m
@@ -37,24 +37,3 @@ surveyLink = 'https://docs.google.com/forms/d/e/1FAIpQLSdg4kv6s4wTSwCwDNBg8obF0-
 web(surveyLink, '-browser')
 
 
-
-%% send behavioral summary to Evernote notebook
-%  sends to Sensors mice notebook as Elsie
-%  appends to note with name of animal
-
-% email configuration 
-setpref('Internet','E_mail','kepecslab.cshl@gmail.com')
-setpref('Internet','SMTP_Server','smtp.gmail.com')
-setpref('Internet','SMTP_Username','kepecslab.cshl@gmail.com')
-setpref('Internet','SMTP_Password','D3cision')
-props = java.lang.System.getProperties;
-props.setProperty('mail.smtp.auth','true');
-props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
-props.setProperty('mail.smtp.socketFactory.port','465');
-
-ENemail = 'egibson214.3941e4d@m.evernote.com';
-
-% note content
-text2add = strcat(sessionString,' ----------------------------------------------------');
-noteTitle = strcat(animalName, ' @Sensors mice +');
-sendmail(ENemail, noteTitle, text2add, {outcomeFullPath});
